0e5602ab226ce65b750337ab7881300d3175f211
Iotalh <marceline.cheng@gmail.com>
2019-01-03 23:37:39 +0800

    update find_p

--- s1072026.cpp
+++ s1072026.cpp
@@ -8,10 +8,12 @@ void find_p(); // 找到要跳的座標
 int distance(); // 計算距離
 void output(); // 輸出
 int color; // 玩家顏色
-int k = 0; // 棋子編號
 const int N = 17;
 int b[N][N]; // 儲存每個座標的值 (eg. 空地、玩家棋子)
 int index; // 棋子編號
+int fin_index;
+int fin_dir;
+int fin_score = -100;
 struct place // (x,y)
 {
 	int x;
@@ -21,16 +23,17 @@ struct place find_pos[4]; // 找到可能可以跳的座標
 struct place pos[15]; // 原始座標
 struct place new_pos; // 可以走到的座標
 struct place fin_pos; // 經過比較之後，確定要走的
+struct place 
 struct place target; // 終點
 int main(int argc, char *argv[])
 {
 	color = *argv[1] - '0'; // 給予顏色代號
 	if (color == 2) // 根據不同顏色給予不同終點
 	{
-		target = { 4,16 }; //y,x
+		target = { 16,4 }; //y,x
 	}
 	else if (color == 3) {
-		target = { 16,4 };
+		target = { 4,16 };
 	}
 	else if (color == 4) {
 		target = { 4,4 };
@@ -57,6 +60,7 @@ void read()
 	for (int i = 0; i < N; i++) { // 儲存每一個自己棋子的座標
 		for (int j = 0; j < N; j++) {
 			if (b[i][j] == color) { //如果該座標是自己的棋子就儲存到pos[]
+				int k = 0;
 				pos[k].x = i;
 				pos[k].y = j;
 				k++; // 給棋子編號
@@ -67,55 +71,35 @@ void read()
 
 void find_p()
 {
-	int min = 600;
-	do
-	{
+	while(還有時間){
 		srand(time(NULL));
-		index = rand() % 15; // 從0~14號棋子隨機選一顆
-		// cout << index << endl;
-		// cout << "(" << pos[index].x << "," << pos[index].y << ")" << endl;
-		find_pos[0].x = (pos[index].x - 1); // 往旁邊找或許能走的位置
-		find_pos[0].y = (pos[index].y + 1);
+		index = rand() % 15;
+		direction = rand() % 6;
+		int dir_x[6] = {0, 1, 1, 0, -1, -1};
+		int dir_y[6] = {-1, -1, 0, 1, 1, 0};
+		dir_x[direction]
 
-		find_pos[1].x = (pos[index].x);
-		find_pos[1].y = (pos[index].y + 1);
+		find_pos.x = (pos[index].x + dir_x[direction]);
+		find_pos.y = (pos[index].y + dir_y[direction]);
 
-		find_pos[2].x = (pos[index].x + 1);
-		find_pos[2].y = (pos[index].y);
-
-		find_pos[3].x = (pos[index].x + 1);
-		find_pos[3].y = (pos[index].y - 1);
-
-		find_pos[4].x = (pos[index].x);
-		find_pos[4].y = (pos[index].y - 1);
-
-		find_pos[5].x = (pos[index].x - 1);
-		find_pos[5].y = (pos[index].y);
-
-	}while(b[find_pos[0].x][find_pos[0].y] != 1 && b[find_pos[1].x][find_pos[1].y] != 1 && b[find_pos[2].x][find_pos[2].y] != 1 && b[find_pos[3].x][find_pos[3].y] != 1 && b[find_pos[4].x][find_pos[4].y] != 1 && b[find_pos[5].x][find_pos[5].y] != 1 );
-	
-	for(int i = 0; i<6; i++){
-		if(find_pos[i].x >= 0 && find_pos[i].y >= 0 && find_pos[i].x < 17 && find_pos[i].y < 17) {
-			if(b[find_pos[i].x][find_pos[i].y] == 1){
-				new_pos.x = find_pos[i].x;
-				new_pos.y = find_pos[i].y;
-				int n = distance();
-				if (min > distance())
-				{
-					min = n;
-					fin_pos.x = new_pos.x;
-					fin_pos.y = new_pos.y;
+		if(find_pos.x >= 0 && find_pos.x < 17 && find_pos.y >= 0 && find_pos.y < 17 ){
+			if(b[find_pos.x][find_pos.y] == 1){ // score 用來紀錄走法的好壞，每一次都要比遠始位置更靠近終點
+				score = distance(pos[index]) - distance(find_pos);
+				if(score > fin_score){
+					fin_index = index;
+					fin_dir = direction;
+					fin_score = score;
 				}
 			}
 		}
-	}
-}
 
-int distance(){
-	int d;
-	d = ((new_pos.x - target.x)*(new_pos.x - target.x)) + ((new_pos.y - target.y)*(new_pos.y - target.y));
-	return d;
-}
+		int distance(struct place a){
+			int d;
+			d = ((a.x - target.x)*(a.x - target.x)) + ((a.y - target.y)*(a.y - target.y));
+			return d;
+		}
+
+	}
 
 void output()
 {
